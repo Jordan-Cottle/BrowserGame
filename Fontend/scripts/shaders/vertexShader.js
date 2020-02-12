9a77fe306bf3ea60ad3c45e9abf3e891061e1b52
JordanCottle <jordancottle622@gmail.com>
2020-02-11 23:05:35 -0500

    add transform to vertex shader

--- Fontend/scripts/shaders/vertexShader.js
+++ Fontend/scripts/shaders/vertexShader.js
@@ -1,11 +1,15 @@
 let vertexShaderSource = `
 attribute vec4 vPosition;
-attribute vec4 vColor; 
+attribute vec4 vColor;
+
+uniform mat4 transform;
 
 varying vec4 fColor;
+
 void main()
 {
 	fColor = vColor;
-	gl_Position = vPosition;
+
+	gl_Position = transform*vPosition;
 }
 `;
